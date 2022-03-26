476f120cd6e4aafe02a7d79809a2b054bcaebb99
Warkinstar <Warkinstar@mail.ru>
2022-03-26 15:45:03 +0300

    'ch14'

--- learning/urls.py
+++ learning/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import BookListView, BookDetailView
-from .views import LearningListView
+from .views import LearningListView, SearchResultsListView
 
 urlpatterns = [
     path('', LearningListView.as_view(), name='learning_list'),
     path('books/', BookListView.as_view(), name='book_list'),
     path('books/<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- learning/views.py
+++ learning/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import TemplateView
 from django.views.generic import ListView, DetailView
@@ -21,3 +22,15 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'learning/book_detail.html'
     login_url = 'account_login'
     permission_required = 'learning.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'learning/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
