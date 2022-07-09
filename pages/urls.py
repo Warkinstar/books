11dcb0aa7f3a97f0c5216aaea86c0ceaa1371165
Ramil <Warkinstar@mail.ru>
2022-07-09 23:27:41 +0600

    teacher page
    teacher page показывает список учителей и их данные

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, TeacherListView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
+    path('teachers/', TeacherListView.as_view(), name='teacher_list'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
 
+group = 'teachers'
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -7,3 +9,14 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class TeacherListView(TemplateView):
+    template_name = 'teacher_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = get_user_model()
+        context['teacher_list'] = obj.objects.filter(groups__name=group)
+        return context
+
