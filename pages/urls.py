baf9f847bd9e253fbf0d38ae30d3918347484a8c
Ramil <Warkinstar@mail.ru>
2023-05-29 01:54:04 +0600

    Manual about site's work

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, TeacherListView
+from .views import HomePageView, AboutPageView, TeacherListView, ManualPageView
 
 urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('teachers/', TeacherListView.as_view(), name='teacher_list'),
-    path('', HomePageView.as_view(), name='home'),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("teachers/", TeacherListView.as_view(), name="teacher_list"),
+    path("manual/", ManualPageView.as_view(), name="manual"),
+    path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,28 @@
 from django.views.generic import TemplateView
 from django.contrib.auth import get_user_model
 
-group = 'teachers'
+group = "teachers"
+
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "pages/home.html"
 
 
 class AboutPageView(TemplateView):
-    template_name = 'about.html'
+    template_name = "pages/about.html"
+
+
+class ManualPageView(TemplateView):
+    """Руководство по сайту"""
+
+    template_name = "pages/manual.html"
 
 
 class TeacherListView(TemplateView):
-    template_name = 'teacher_list.html'
+    template_name = "pages/teacher_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         obj = get_user_model()
-        context['teacher_list'] = obj.objects.filter(groups__name=group)
+        context["teacher_list"] = obj.objects.filter(groups__name=group)
         return context
-
